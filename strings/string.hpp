cea0a8dcc60873be8d94d24185d26afa72ad64f4
hwadii <wadii.hajji@etu.univ-amu.fr>
2019-12-14 00:10:25 +0100

    Formatting

--- strings/string.hpp
+++ strings/string.hpp
@@ -1,34 +1,37 @@
+#include <cstdarg>
 #include <iostream>
-#include <vector>
-#include <string>
 #include <istream>
 #include <ostream>
-#include <cstdarg>
+#include <string>
+#include <vector>
 
-class String
-{
-  private:
-    std::string _el;
+class String {
+private:
+  std::string _el;
 
-  public:
-    String() : _el(""), len(0) {};
-    /* String(const String&); */
-    String(std::string el) : _el(el), len(el.size()) {};
-    ~String() {};
-    inline std::string get_string() const { return _el; } // ✅
-    String char_at(int); // ✅
-    std::size_t index_of(const String&); // ✅
-    bool includes(const String&); // ✅
-    String substring(size_t, size_t); // ✅
-    std::vector<String> split(const String&); // ✅
-    String concat(int, ...);
-    friend bool operator==(const String &s1, const String &s2) {return s1.get_string() == s2.get_string();}
-    friend bool operator!=(const String &s1, const String &s2) { return !(s1 == s2); }
-    String& operator+=(const String&);
-    String& operator=(const std::string&);
-    friend std::ostream& operator<<(std::ostream&, const String&);
-    friend std::ostream& operator<<(std::ostream&, const std::vector<String>&);
-    friend std::istream& operator>>(std::istream&, String&);
+public:
+  String() : _el(""), len(0){};
+  /* String(const String&); */
+  String(std::string el) : _el(el), len(el.size()){};
+  ~String(){};
+  inline std::string get_string() const { return _el; } // ✅
+  String char_at(int);                                  // ✅
+  std::size_t index_of(const String &);                 // ✅
+  bool includes(const String &);                        // ✅
+  String substring(size_t, size_t);                     // ✅
+  std::vector<String> split(const String &);            // ✅
+  String concat(int, ...);
+  friend bool operator==(const String &s1, const String &s2) {
+    return s1.get_string() == s2.get_string();
+  }
+  friend bool operator!=(const String &s1, const String &s2) {
+    return !(s1 == s2);
+  }
+  String &operator+=(const String &);
+  String &operator=(const std::string &);
+  friend std::ostream &operator<<(std::ostream &, const String &);
+  friend std::ostream &operator<<(std::ostream &, const std::vector<String> &);
+  friend std::istream &operator>>(std::istream &, String &);
 
-    int len;
+  int len;
 };
