55b1226868af179b4d2ba07627094957fa9ccc3d
Wadii Hajji <wadii.hajji@etu.univ-amu.fr>
2020-08-29 17:03:56 +0100

    Much better because I kinda copied stdlib impl
    Now do it in rust.

--- relative-import-rb/tests/test_relative_import.rb
+++ relative-import-rb/tests/test_relative_import.rb
@@ -3,26 +3,33 @@ require_relative '../lib/lib'
 require 'test/unit'
 
 class TestRelativeImport < Test::Unit::TestCase
-  def test_common_parent
-    ri = RelativeImport.new(source: Pathname.new('./bin/main.js'), target: Pathname.new('./lib/lib.js'))
-    assert_equal(ri.common_parent, '.')
-    ri.source = Pathname.new('./lib/math/math.js')
-    assert_equal(ri.common_parent, 'lib')
-  end
-
   def test_build_relative_simple
     ri = RelativeImport.new(source: Pathname.new('./lib/math/math.js'), target: Pathname.new('./lib/math/lib.js'))
-    p ri.compute_dots
-    assert_equal(ri.build_relative, Pathname.new('./lib.js'))
+    assert_equal(ri.build_relative, Pathname.new('lib.js'))
+  end
+
+  def test_build_relative_simple_2
+    ri = RelativeImport.new(source: Pathname.new('./main.js'), target: Pathname.new('./lib.js'))
+    assert_equal(ri.build_relative, Pathname.new('lib.js'))
+  end
+
+  def test_build_relative_complex
+    ri = RelativeImport.new(source: Pathname.new('./lib/main/main/main/main.js'), target: Pathname.new('./util.js'))
+    assert_equal(ri.build_relative, Pathname.new('../../../../util.js'))
+  end
+
+  def test_build_relative_complex_2
+    ri = RelativeImport.new(source: Pathname.new('./lib/main/main/main/main.js'), target: Pathname.new('./lib/main/main/math/math.js'))
+    assert_equal(ri.build_relative, Pathname.new('../math/math.js'))
   end
 
   def test_build_relative_deep
     ri = RelativeImport.new(source: Pathname.new('./lib/math/math.js'), target: Pathname.new('./lib/lib.js'))
-    assert_equal(ri.build_relative, Pathname.new('../../lib.js'))
+    assert_equal(ri.build_relative, Pathname.new('../lib.js'))
   end
 
-  def test_compute_rest
+  def test_compute_dots
     ri = RelativeImport.new(source: Pathname.new('./lib/math/math.js'), target: Pathname.new('./lib/lib.js'))
-    assert_equal(ri.compute_rest, 'lib.js')
+    assert_equal(ri.compute_dots, ['..'])
   end
 end
