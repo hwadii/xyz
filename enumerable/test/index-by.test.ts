65659379b7bdc649a79017f384f4d27c100c4d15
Wadii Hajji <wadii@cardiologs.com>
2021-02-07 17:05:58 +0100

    Add pluck

---
+++ enumerable/test/index-by.test.ts
@@ -0,0 +1,7 @@
+// @ts-expect-error
+import { indexBy } from "../source/index-by";
+import test from "ava";
+
+test.todo("creates an object with each index as keys");
+
+test.todo("creates an empty object when input is empty");
