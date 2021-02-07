ce90c12e30245b33f110f54a60dc518e6058184a
Wadii Hajji <wadii@cardiologs.com>
2021-02-07 16:05:16 +0100

    More typesafe index-with
    Add tests.

--- enumerable/source/index-with.ts
+++ enumerable/source/index-with.ts
@@ -1,12 +1,12 @@
-export function indexWith<T extends string | number | symbol>(
+export function indexWith<T extends string | number | symbol, U extends unknown>(
   list: T[],
-  index: unknown
-): Record<T, unknown> {
+  index: U
+): Record<T, U> {
   return list
     .map((elem) => [elem, index] as const)
     .reduce((acc, curr) => {
       const [elem, index] = curr;
       acc[elem] = index;
       return acc;
-    }, {} as Record<T, unknown>);
+    }, {} as Record<T, U>);
 }
--- enumerable/test/index-with.test.ts
+++ enumerable/test/index-with.test.ts
@@ -1,12 +1,19 @@
 import { indexWith } from "../source/index-with";
 import test from "ava";
 
-test("creates an object with array elements as key and index as value", (t) => {
-  const result = indexWith(["hello", "world"], 2);
-  t.deepEqual(result, { hello: 2, world: 2 });
+test("creates an object with array elements as key and number index as value", (t) => {
+  t.deepEqual(indexWith(["hello", "world"], 2), { hello: 2, world: 2 });
+});
+
+test("creates an object with array elements as key and string index as value", (t) => {
+  t.deepEqual(indexWith(["hello", "world"], "foobar"), { hello: "foobar", world: "foobar" });
+});
+
+test("creates an object with array elements as key and non primitive type index as value", (t) => {
+  const nonPrimitive = { foo: "bar", bar: "foo" };
+  t.deepEqual(indexWith(["hello", "world"], nonPrimitive), { hello: nonPrimitive, world: nonPrimitive });
 });
 
 test("creates an empty object with empty array", (t) => {
-  const result = indexWith([], 2);
-  t.deepEqual(result, {});
+  t.deepEqual(indexWith([], 2), {});
 });
