9d38544e0ea23f65bb8dcd46efbee3ef61024871
Wadii Hajji <wadii@cardiologs.com>
2021-02-07 20:06:11 +0100

    Add missing test to index-by

--- enumerable/test/index-by.test.ts
+++ enumerable/test/index-by.test.ts
@@ -1,7 +1,13 @@
-// @ts-expect-error
 import { indexBy } from "../source/index-by";
 import test from "ava";
 
-test.todo("creates an object with each index as keys");
-
-test.todo("creates an empty object when input is empty");
+test("creates an object with each index as keys", (t) => {
+  const input = [
+    { x: 1, y: "hello" },
+    { x: 2, y: "world" },
+  ];
+  t.deepEqual(indexBy(input, "x"), {
+    1: { x: 1, y: "hello" },
+    2: { x: 2, y: "world" },
+  });
+});
