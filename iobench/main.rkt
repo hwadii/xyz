c25ccec93e423ac2a1a3dd868522ea6c846a99b9
Wadii Hajji <wadii@cardiologs.com>
2021-09-19 23:38:21 +0200

    --wip--: it's slow

--- iobench/main.rkt
+++ iobench/main.rkt
@@ -1,8 +1,15 @@
 #lang racket
 
-(define grid (open-input-file "./letters.txt"))
-(define mask (open-input-file "./mask.txt"))
-(define ref (string-normalize-spaces (port->string (open-input-file "ref.txt"))))
+(require racket/math)
+
+(define grid (string-split (port->string (open-input-file "./small-letters.txt")) "\n"))
+(define mask (string-split (port->string (open-input-file "./small-mask.txt")) "\n"))
+#| (define ref (string-normalize-spaces (port->string (open-input-file "ref.txt")))) |#
+
+; Return a list of pairs
+(define (ranges max-nr by)
+  (let ([step (/ max-nr by)])
+    (map (lambda (nr) (cons nr (+ step nr))) (range 0 max-nr step))))
 
 (define (to-string grid-row mask-row)
   (define (to-char c m)
@@ -12,11 +19,19 @@
      [m (regexp-match* #rx"." mask-row)])
     (string-append word (to-char c m))))
 
-(define result (for/fold ([entire-word ""])
-                 ([grid-row (in-lines grid)]
-                  [mask-row (in-lines mask)])
-                 (string-append entire-word (to-string grid-row mask-row))))
+(define (to-word grid-slice mask-slice)
+  (for/fold ([entire-word ""])
+    ([grid-row grid-slice]
+     [mask-row mask-slice])
+    (string-append entire-word (to-string grid-row mask-row))))
 
-(print (equal? (sha1-bytes (string->bytes/utf-8 result)) (sha1-bytes (string->bytes/utf-8 ref))))
+#| (print (equal? (sha1-bytes (string->bytes/utf-8 result)) (sha1-bytes (string->bytes/utf-8 ref)))) |#
+
+(define (to-slice start-end)
+  (to-word ()))
+
+(map )
+
+(print (to-word grid mask))
 
 ; TODO(wadii): Make this multithreaded.
